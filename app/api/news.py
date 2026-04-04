9b967a536cfc164b5a7db025484a9a48a25e2218
Qobilov Ozodbek <121813270+QobilovOzodbek@users.noreply.github.com>
2026-04-04 22:18:14 +0500

    Enhance news scraping functionality and add detail endpoint

--- app/api/news.py
+++ app/api/news.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
-from app.services.scraper import get_news
+from app.services.scraper import get_news, get_news_detail
 
 router = APIRouter(prefix="/news", tags=["News"])
 
 @router.get("/")
 def news():
-    return {"news": get_news()}
+    return get_news()
+
+
+@router.get("/detail")
+def news_detail(url: str):
+    return get_news_detail(url)
--- app/services/scraper.py
+++ app/services/scraper.py
@@ -1,6 +1,67 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "https://bstu.uz"
+
+
 def get_news():
-    # Hozircha fake data
-    return [
-        {"title": "Yangi grant e'lon qilindi"},
-        {"title": "Talabalar uchun tadbir bo‘lib o‘tadi"}
-    ]
+    url = f"{BASE_URL}/articles/yangilik"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    news_list = []
+
+    items = soup.find_all("div", class_="tt_newsitem")
+
+    for item in items:
+        try:
+            title_tag = item.find("h3").find("a")
+            title = title_tag.text.strip()
+            link = title_tag["href"]
+
+            img_tag = item.find("img")
+            image = img_tag["src"] if img_tag else None
+
+            date_tag = item.find("span", class_="dates")
+            date = date_tag.text.strip() if date_tag else None
+
+            news_list.append({
+                "title": title,
+                "link": link,
+                "image": image,
+                "date": date
+            })
+
+        except Exception as e:
+            print("Error parsing item:", e)
+
+    return news_list
+
+def get_news_detail(url: str):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # TITLE
+    title_tag = soup.find("h1")
+    title = title_tag.text.strip() if title_tag else None
+
+    # CONTENT
+    paragraphs = soup.select("article.post-content p")
+    content = "\n".join([p.text.strip() for p in paragraphs if p.text.strip()])
+
+    # IMAGES
+    images = []
+    slides = soup.select(".mySlides img")
+
+    for img in slides:
+        src = img.get("src")
+        if src:
+            if src.startswith("/"):
+                src = BASE_URL + src
+            images.append(src)
+
+    return {
+        "title": title,
+        "content": content,
+        "images": images
+    }
